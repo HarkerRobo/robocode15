3dbe2243d348a6426db24412f56cb2d98abea047
Andrew Tierno <andrew.tierno@gmail.com>
2015-01-06 17:53:25 -0800

    Initial commit

---
+++ RobotCode2015/src/org/harker/robotics/subsystems/Drivetrain.java
@@ -0,0 +1,19 @@
+package org.harker.robotics.subsystems;
+
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Drivetrain extends Subsystem {
+	private RobotDrive robotDrive;
+	
+	
+	public Drivetrain() {
+		
+	}
+	
+	@Override
+	protected void initDefaultCommand() {
+		
+	}
+
+}
